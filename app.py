175cfcbdc041c649380631461eac13573d3b6e15
Neelesh Vashist <neeleshvashist@gmail.com>
2022-08-20 13:02:01 -0400

    Adding Full Project (20/08/2022)

---
+++ app.py
@@ -0,0 +1,139 @@
+from flask import Flask, flash, request, redirect, url_for, render_template
+import urllib.request
+import os
+from werkzeug.utils import secure_filename
+import cv2
+import pickle
+import imutils
+import sklearn
+from tensorflow.keras.models import load_model
+# from pushbullet import PushBullet
+import joblib
+import numpy as np
+from tensorflow.keras.applications.vgg16 import preprocess_input
+
+
+covid_model = load_model('models/covid.h5')
+diabetes_model = pickle.load(open('models/diabetes.sav', 'rb'))
+breastcancer_model = joblib.load('models/cancer_model.pkl')
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "secret key"
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+
+@app.route('/covid')
+def covid():
+    return render_template('covid.html')
+
+
+@app.route('/breastcancer')
+def breast_cancer():
+    return render_template('breastcancer.html')
+
+
+@app.route('/diabetes')
+def diabetes():
+    return render_template('diabetes.html')
+
+
+###################################################################
+
+
+@app.route('/resultc', methods=['POST'])
+def resultc():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        age = request.form['age']
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Image successfully uploaded and displayed below')
+            img = cv2.imread('static/uploads/'+filename)
+            img = cv2.resize(img, (224, 224))
+            img = img.reshape(1, 224, 224, 3)
+            img = img/255.0
+            pred = covid_model.predict(img)
+            if pred < 0.5:
+                pred = 0
+            else:
+                pred = 1
+            return render_template('resultc.html', filename=filename, fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+        else:
+            flash('Allowed image types are - png, jpg, jpeg')
+            return redirect(request.url)
+
+
+@app.route('/resultd', methods=['POST'])
+def resultd():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        pregnancies = request.form['pregnancies']
+        glucose = request.form['glucose']
+        bloodpressure = request.form['bloodpressure']
+        insulin = request.form['insulin']
+        bmi = request.form['bmi']
+        diabetespedigree = request.form['diabetespedigree']
+        age = request.form['age']
+        skinthickness = request.form['skin']
+        pred = diabetes_model.predict(
+            [[insulin, bmi, diabetespedigree, age]])
+        return render_template('resultd.html', fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+
+@app.route('/resultbc', methods=['POST'])
+def resultbc():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        age = request.form['age']
+        cpm = request.form['concave_points_mean']
+        am = request.form['area_mean']
+        rm = request.form['radius_mean']
+        pm = request.form['perimeter_mean']
+        cm = request.form['concavity_mean']
+        pred = breastcancer_model.predict(
+            np.array([cpm, am, rm, pm, cm]).reshape(1, -1))
+        return render_template('resultbc.html', fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+
+# No caching at all for API endpoints.
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
